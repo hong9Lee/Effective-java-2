ed30a20b5d6a1f327352c00c79e200c264a85d08
LeeHongGyu <ehong9@growcorp.co.kr>
2023-01-11 17:23:22 +0900

    item 21. 인터페이스는 구현하는 쪽을 생각해 설계하라. commit

---
+++ src/main/java/effectivejavapart2/study/chapter04/item21/SubClass.java
@@ -0,0 +1,10 @@
+package effectivejavapart2.study.chapter04.item21;
+
+public class SubClass extends SuperClass implements MarkerInterface {
+
+    public static void main(String[] args) {
+        SubClass subClass = new SubClass();
+        subClass.hello();
+    }
+
+}
---
+++ src/main/java/effectivejavapart2/study/chapter04/item21/SuperClass.java
@@ -0,0 +1,8 @@
+package effectivejavapart2.study.chapter04.item21;
+
+public class SuperClass {
+
+    private void hello() {
+        System.out.println("hello class");
+    }
+}
